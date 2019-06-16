b5bc57b7f9055b1a0fe90a7b4606642aabf98ffe
Star Platinum <37128782+18511547952@users.noreply.github.com>
2019-06-16 21:46:19 +0800

    Update WenClient.java

--- src/main/java/com/wenhf/niochatroom/service/WenClient.java
+++ src/main/java/com/wenhf/niochatroom/service/WenClient.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class WenClient {
     public static void main(String[] args) throws IOException {
-        new NIOClient("文红枫").start();
+        new NIOClient("文").start();
     }
 }
